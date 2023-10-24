d911cc22a84fa1d44305de8b75a3984564230629
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2023-10-24 14:32:25 +0100

    step 2 done

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,11 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sessions',
 ]
 
 MIDDLEWARE = [
