2928aec30b6d6a55c6c01eb6680ac95cfc84155b
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2023-10-24 22:31:42 +0100

    step 3 done

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
-# Create your views here.
+from .models import Question
+from django.template import loader
+from django.http import Http404
 
 def index(request):
-    return HttpResponse('Hello World')
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    # There’s also a get_list_or_404() function, which works just as get_object_or_404()
+    # except using filter() instead of get(). It raises Http404 if the list is empty.
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/details.html", {"question": question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s. " % question_id)
